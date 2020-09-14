7d45f1c2e0e0ec7be153ade81a087744afbb4761
Mouhsen Ibrahim <mohsen47@hotmail.co.uk>
2020-09-14 13:50:08 +0300

    Add hello route

---
+++ sample/hello/urls.py
@@ -0,0 +1,6 @@
+from .views import hello
+from django.urls import path
+
+urlpatterns = [
+    path('', hello)
+]
--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lz=2s&2ijts091f#5^t7#tjcy!arv9yvi-b%v&7pglks3320nq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["68.183.219.10",
+ALLOWED_HOSTS = ["68.183.219.10", "localhost",
                  "sample-python-genyb.ondigitalocean.app"]
 
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello',
 ]
 
 MIDDLEWARE = [
