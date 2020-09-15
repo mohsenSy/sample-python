40226adac8f0c8b28caffd2b5929a9ce593b058a
Mouhsen Ibrahim <mohsen47@hotmail.co.uk>
2020-09-15 22:59:09 +0300

    Add index page

--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('index.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
 ]
